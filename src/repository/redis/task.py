2dec77ba3a790014a10f306fe5d60ffb2c7ebf2b
ElCresp0 <jacky8203@protonmail.com>
2024-12-01 02:52:43 +0100

    add redis

--- src/repository/redis/task.py
+++ src/repository/redis/task.py
@@ -1,12 +1,49 @@
-from celery.result import AsyncResult
+import celery
 
-#TODO użyyć klase TaskEntity i  zwracać więcej informacji
+from celery.result import AsyncResult
+from fastapi import HTTPException
+from redis import Redis
+
+from ...entity.task import TaskEntity
+
 class TaskRepository:
-    def get_task(self, task_id: str) -> dict | None:
-        task_result = AsyncResult(task_id)
-        if(task_result):
-            return {'task_id': task_id, 'status': task_result.status, 'result': task_result.result}
+    def __init__(self):
+        redis_url = celery.conf.result_backend
+        self.client = Redis.from_url(redis_url)
+
+    def create_task(self, task: TaskEntity) -> bool:
+        """
+        ta i pozostałe metody: go async?
+        """
+        task_id = task.id
+        if self.client.get(task_id) == None: # or task.app.AsyncResult(task_id).ready() # https://derlin.github.io/introduction-to-fastapi-and-celery/03-celery/
+            task_dict = task.__dict__.copy()
+            # task_dict.pop('callable', None)
+            self.client.set(task.id, task_dict)
         else:
-            None
-    
-    
+            raise HTTPException(400, "Task with given id already exists")
+        return True
+
+    def update_task(self, task: TaskEntity) -> bool:
+        existing_task = self.client.get(task.id)
+        if not existing_task:
+            raise HTTPException(404, "Task with given id doesn't exists")
+
+        task_dict = task.__dict__.copy()
+        # task_dict.pop('callable', None)
+        self.client.set(task.id, task_dict)
+
+        return True
+
+    def get_task(self, task_id: str) -> TaskEntity | None:
+        task = self.client.get(task_id)
+        if not task:
+            raise HTTPException(404, "Task with given id doesn't exists")
+        return TaskEntity(**task) # nice
+
+    # def get_task(self, task_id: str) -> dict | None:
+    #     task_result = AsyncResult(task_id)
+    #     if(task_result):
+    #         return {'task_id': task_id, 'status': task_result.status, 'result': task_result.result}
+    #     else:
+    #         None
