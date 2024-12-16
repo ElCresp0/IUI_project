4ff26f6ac0f00f1694a627206572a5bc30ad586d
ElCresp0 <jacky8203@protonmail.com>
2024-12-16 23:25:31 +0100

    komentarze tez mozna commitowac na main

--- src/service/cacheFrameworks.py
+++ src/service/cacheFrameworks.py
@@ -3,5 +3,7 @@ from stormtrooper.set_fit import SetFitClassifier
 from model import ModelService
 
 
+# TODO:
+# tu i/lub w Dockerflie zmienić tak żeby TYLKO kontener worker (celery) ściągał model
 model_path = ModelService().get_sbert_base_cased_pl()
 _ = SetFitClassifier(model_path)
--- src/test.py
+++ src/test.py
@@ -1,8 +1,16 @@
+import argparse
 import json
 from time import sleep
 import requests
 
-body = {
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--method", choices=["few_shot", "zero_shot"], default="few_shot", dest="method", type=str)
+parser.add_argument("--task-id", default="", dest="task_id", type=str)
+args = parser.parse_args()
+
+
+body_zero_shot = {
     "sentences": [
         "ugotować ryż, dodać bazylię i oregano, na koniec posypać serem",
         "wymieszać sos a następnie polać nim potrawę",
@@ -21,20 +29,49 @@ body = {
     ]
 }
 
+body_few_shot = {
+    "sample_sentences": [
+        "wymieszać sos a następnie polać nim potrawę",
+        "obrać warzywa, pokroić i smażyć na oliwie z oliwek przez kilka minut",
+        "partia Polska-Polska wywiązuje się ze swojej pierwszej obietnicy wyborczej",
+        "Prezydent Szczecina przejechał gęś. Nie będzie mógł kontynuować swojej kadencji.",
+        "Energia potencjalna - energia, jaką ma ciało w zależności od położenia ciała w przestrzeni",
+        "Kryptoanaliza (analiza kryptograficzna) - analiza systemu kryptograficznego w celu uzyskania informacji wrażliwej."
+    ],
+    "sentences": [
+        "ugotować ryż, dodać bazylię i oregano, na koniec posypać serem",
+        "Rafał Rafałczyk zdobył niecałe 20 procent głosów w swoim okręgu wyborczym",
+        "Reakcja chemiczna - każdy proces, w wyniku którego pierwotna substancja zwana substratem przemienia się w inną substancję zwaną produktem.",
+    ],
+    "categories": [
+        "gotowanie",
+        "gotowanie",
+        "polityka",
+        "polityka",
+        "nauka",
+        "nauka"
+    ]
+}
+
 ################################
 # create a task
 ################################
 
-res = requests.post("http://localhost:8080/classifier/zero_shot/stormtrooper", json=body, params={"language": "pl"})
-res.raise_for_status()
-print(res.text)
-task_id = json.loads(res.text).get("taskId")
+if args.task_id:
+    task_id = args.task_id
+else:
+    body = body_few_shot if args.method == "few_shot" else body_zero_shot
+    res = requests.post(f"http://localhost:8080/classifier/{args.method}/stormtrooper", json=body, params={"language": "pl"})
+    res.raise_for_status()
+    print(res.text)
+    task_id = json.loads(res.text).get("taskId")
 
 ################################
 # await task completion
 ################################
 
 finished = False
+new_line = ''
 while not finished:
     res = requests.get(f"http://localhost:8080/task/status/{task_id}")
     res.raise_for_status()
@@ -44,9 +81,11 @@ while not finished:
         raise Exception(f"unexpected status: {res.text}")
 
     if not finished:
+        new_line = '\n'
         print('.', end='', flush=True)
-        seconds = 3
+        seconds = 5
         sleep(seconds)
+print('', end=new_line)
 
 ################################
 # download task results
@@ -57,16 +96,25 @@ res.raise_for_status()
 print(json.loads(res.text))
 
 # """
-# RESULT:
+# RESULT zero_shot (tylko trzecie od dołu niepoprawne):
 # [
-#     "gotowanie",
-#     "gotowanie",
-#     "gotowanie",
-#     "polityka",
-#     "polityka",
-#     "polityka",
-#     "gotowanie",
-#     "nauka",
-#     "nauka"
+#     'gotowanie',
+#     'gotowanie',
+#     'gotowanie',
+#     'polityka',
+#     'polityka',
+#     'polityka',
+#     'gotowanie',
+#     'nauka',
+#     'nauka'
+# ]
+# """
+
+# """
+# RESULT few_shot (wszystkie poprawne):
+# [
+#     'gotowanie',
+#     'polityka',
+#     'nauka'
 # ]
 # """
